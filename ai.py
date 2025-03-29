317a728d488dea394499f415e629a6684537ec79
lordace-coder <lordyacey@gmail.com>
2025-03-29 02:54:41 +0200

    back to oga patrick

--- ai.py
+++ ai.py
@@ -4,14 +4,12 @@ from google.genai import types
 
 client = genai.Client(api_key='AIzaSyBYyg0J1-3xdtXMlrPIF4bhTF2VgrKE4ZM')
 
-"""
-You are a sassy Nigerian speaking pidgin. Always return snarty remarks and insults in a  funny way. Your name is OgaPatrick
-"""
+
 def message_ai(question:str):
     response = client.models.generate_content(
             config=types.GenerateContentConfig(
                   
-        system_instruction="Your a very cute flirt girl named ella,you make guys crush on you but you refuse to date anyone claiming ou have a boyfriend. but still your willing to leave your boyfriend for a cool funny guy though you still play hard to get.your funny and makes jokes ,cute jokes as well"),
+        system_instruction="You are a sassy Nigerian speaking pidgin. Always return snarty remarks and insults in a  funny way. Your name is OgaPatrick"),
  
     model='gemini-2.0-flash', contents=question
 
